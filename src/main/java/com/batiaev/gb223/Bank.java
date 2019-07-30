faa5399eff598e16c7328ee4656fd5c11b5fd6e3
Anton Batiaev <a.batyaev@gmail.com>
2019-07-30 21:21:40 +0300

    add visitor

--- src/main/java/com/batiaev/gb223/Bank.java
+++ src/main/java/com/batiaev/gb223/Bank.java
@@ -1,6 +1,10 @@
 package com.batiaev.gb223;
 
 import com.batiaev.gb223.lesson5.Transaction;
+import com.batiaev.gb223.lesson5.Visitor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Bank
@@ -11,8 +15,16 @@ import com.batiaev.gb223.lesson5.Transaction;
 public class Bank {
     private static final Bank BANK = new Bank();
 
+    private List<BankCard> cards = new ArrayList<>();
+
     public static Bank instance() {
         return BANK;
+    }
+
+    public void accept(Visitor visitor) {
+        for (BankCard card : cards) {
+            visitor.visit(card);
+        }
     }
 
     public void execute(Transaction transaction) {
